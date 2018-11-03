fb0bad93b9f6b31251e399812c6e9a77ca690905
MrMarnic <marceljedig@web.de>
2018-11-03 01:57:07 +0100

    +Main Class created

--- src/main/java/com/cqr/main/ChocolateQuestRepoured.java
+++ src/main/java/com/cqr/main/ChocolateQuestRepoured.java
@@ -1,18 +1,18 @@
-package com.example.examplemod;
+package com.cqr.main;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ChocolateQuestRepoured.MODID, name = ChocolateQuestRepoured.NAME, version = ChocolateQuestRepoured.VERSION)
+public class ChocolateQuestRepoured
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "chocolatequestrepoured";
+    public static final String NAME = "ChocolateQuestRepoured";
+    public static final String VERSION = "0.1a";
 
     private static Logger logger;
 
@@ -25,7 +25,12 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
     }
 }
