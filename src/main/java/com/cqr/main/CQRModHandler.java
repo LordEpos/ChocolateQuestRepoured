7ce77d3ad7c65393cef825f1c40e9a67ed729bf3
MrMarnic <marceljedig@web.de>
2018-12-03 22:11:55 +0100

    + Texture Registration

--- src/main/java/com/cqr/main/CQRModHandler.java
+++ src/main/java/com/cqr/main/CQRModHandler.java
@@ -1,9 +1,22 @@
 package com.cqr.main;
 
 import me.marnic.modapi.handler.ModHandler;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
 
 /**
  * Created by Marcel on Dez,2018
  */
 public class CQRModHandler extends ModHandler {
+    @Override
+    public void registerBlocks(RegistryEvent.Register<Block> event) {
+
+    }
+
+    @Override
+    public void registerItems(RegistryEvent.Register<Item> event) {
+
+    }
 }
